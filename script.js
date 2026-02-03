00931a348c6a131f9a67675e718f9d556fffa2c8
KaelMcchickenMan <23218km@hvhs.school.nz>
2026-02-03 15:05:02 +1300

    save

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let Points = 0
-   function AddPoint() {
-      document.getElementById("PointsCounter").value = Points;
-    }
+
 function ScriptAlert() {
 document.getElementById("WelcomeMessage").innerHTML = "you pressed buton"
 }
 
 function AddPoint() {
 Points + 1
+document.getElementById("PointsCounter").value = Points;
 }
