3d337f9cc665822c65e5ed5abdbc8ae0274e7c9d
KaelMcchickenMan <23218km@hvhs.school.nz>
2026-02-03 14:53:04 +1300

    yess

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-alert("Congratulations!\nYou got the script running.")
-
+let Points = 0
+document.getElementById("PointsCounter").innerHTML = Points;
 function ScriptAlert() {
 document.getElementById("WelcomeMessage").innerHTML = "you pressed buton";
+}
+
+function AddPoint() {
+Points + 1
 }
