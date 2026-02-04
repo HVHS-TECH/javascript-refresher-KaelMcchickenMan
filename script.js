cce902e7f3537e8d50def08662a35091202119d8
KaelMcchickenMan <23218km@hvhs.school.nz>
2026-02-05 10:17:49 +1300

    test

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var Points = 0
 var PointsPerClick = 1
+var UpgradeCost = 10
+
+setInterval(() => { DisplayCost()}, 1000);
 
 function ScriptAlert() {
 document.getElementById("WelcomeMessage").innerHTML = "you pressed buton"
@@ -11,5 +14,15 @@ document.getElementById("PointsCounter").innerHTML = Points
 }
 
 function UpgradePoint() {
-PointsPerClick = PointsPerClick + 1
+    if (Points >= UpgradeCost) {
+    Points = Points - UpgradeCost
+    UpgradeCost = UpgradeCost * 2
+    PointsPerClick = PointsPerClick + 1
+    } else {
+        alert("Cannot Afford")
+    }
+}
+
+function DisplayCost() {
+    document.getElementById("PointsPerClickCost").innerHTML = UpgradeCost
 }
